f24c80428ac2b11852bbdc4cfa88373c13d96278
Tuan Pham <tuanbacyen@gmail.com>
2018-01-26 09:07:11 +0700

    fix first commit

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,9 +6,46 @@ class Product < ApplicationRecord
   has_many :orderDetails
   has_many :rates
 
+
   validates :name,presence: true, length: {maximum: 10}
   validates :content,presence: true, length: {maximum: 100}
   VALID_PRICE_REGEX = /\A(\$)?(\d+)(\.|,)?\d{0,2}?\z/
   validates :price,presence: true,format: {with: VALID_PRICE_REGEX },uniqueness: { case_sensitive: false }
+
+  def self.search search
+    if search
+      where("name LIKE ? or keys LIKE ?", "%#{search}%", "%#{search}%")
+    else
+      all
+    end
+  end
+
+  def self.filtersearch search, trademark, assort, max, min
+    fil = ""
+    if search
+      where("name LIKE ? or keys LIKE ?", "%#{search}%", "%#{search}%")
+    elsif !search
+      if !trademark.blank?
+        fil = fil + " trademark_id = #{trademark} and"
+      end
+      if !assort.blank?
+        fil = fil + " assort_id = #{assort} and"
+      end
+      if !max.blank?
+        fil = fil + " price <= #{max} and"
+      end
+      if !min.blank?
+        fil = fil + " price >= #{min}"
+      else
+        fil = fil[0..(fil.length - 4)]
+      end
+
+      if fil.blank?
+        all
+      else
+        where(fil)
+      end
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :products
-  root "static_pages#home"
+  get "show", to: "show_product#show", as: "showp"
+  get "show/:id", to: "show_product#detail", as: "detailp"
+  get "cart/", to: "show_product#cart", as: "listcart"
+  post "cart/", to: "show_product#destroy", as: "destroycart"
   get "products/new", to: "products#new"
+
+  root "show_product#show"
 end
