d9c2a4c1ac2e753549f2e69576a28d6705da415f
Tuan Pham <tuanbacyen@gmail.com>
2018-01-26 11:00:31 +0700

    delete file of hieu

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,11 +6,6 @@ class Product < ApplicationRecord
   has_many :orderDetails
   has_many :rates
 
-  validates :name,presence: true, length: {maximum: 10}
-  validates :content,presence: true, length: {maximum: 100}
-  VALID_PRICE_REGEX = /\A(\$)?(\d+)(\.|,)?\d{0,2}?\z/
-  validates :price,presence: true,format: {with: VALID_PRICE_REGEX },uniqueness: { case_sensitive: false }
-
   def self.search search
     if search
       where("name LIKE ? or keys LIKE ?", "%#{search}%", "%#{search}%")
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
-
   get "show", to: "show_product#show", as: "showp"
   get "show/:id", to: "show_product#detail", as: "detailp"
   get "cart/", to: "show_product#cart", as: "listcart"
   post "cart/", to: "show_product#destroy", as: "destroycart"
-  get "products/new", to: "products#new"
 
   root "show_product#show"
 end
