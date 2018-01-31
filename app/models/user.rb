49892a4fc485e217c31de0af55fb8bfd13d707fb
ngoquangthang154 <ngoquangthang154@gmail.com>
2018-01-31 08:44:33 +0700

    login_thang

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,26 @@ class User < ApplicationRecord
   has_many :comments
   has_many :orders
   has_many :rates
+
+  validates :username, presence: true, length:{maximum: Setings.maximum_username}
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, length:{maximum: Setings.maximum_email},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+
+  has_secure_password
+  validates :password, presence: true,
+    length: {maximum: Setings.maximum_pass},
+    allow_nil: true
+  validates :name, presence: true, length: {maximum: Setings.maximum_name}
+
+  VALID_PHONE_REGEX = /\d[0-9]\)*\z/
+  validates :phone, presence: true, numericality: true, 
+    length: {maximum: Setings.maximum_phone}, 
+    format: {with: VALID_PHONE_REGEX }
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  get "sessions/new"
+  get "session/new"
+
+  resources :users
+  get "/signup", to: "users#new"
+  get "/showu", to: "users#show", as: "showu"
+  
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+
+  resources :products
   get "show", to: "show_product#show", as: "showp"
   get "show/:id", to: "show_product#detail", as: "detailp"
-  post "show/:id", to: "show_product#detail", as: "reply"
   get "cart/", to: "show_product#cart", as: "listcart"
   post "cart/", to: "show_product#destroy", as: "destroycart"
-  post "postcomment/", to: "show_product#postcomment", as: "postcmt"
 
   root "show_product#show"
 end
